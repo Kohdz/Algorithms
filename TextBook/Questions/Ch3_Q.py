bf310e2ca99c4bcf6f26a0f12e0df285053d45d8
kohdz <umair.stem@gmail.com>
2019-09-27 09:34:34 -0400

    first commit

---
+++ TextBook/Questions/Ch3_Q.py
@@ -0,0 +1,68 @@
+# 1. Implement an Algorithm that balances the following parentheses
+# 	→ ((())) and (()()())
+# 	→ must use a stack
+
+    # Code Goes Here
+
+
+# 2.  Write a helper function to make the balanced parenthesis problem more general
+# 	→ { { ( [ ] [ ] ) } ( ) }
+# 	→  [ [ { { ( ( ) ) } } ] ]
+# 	→  ( ( ( ) ] ) )
+# 	→  [ ] [ ] [ ] ( ) { }
+
+    # Code Goes Here
+
+
+# 3.  Implement a Decimal to Binary Algorithm
+
+    # Code Goes Here
+
+
+# 4.  Implement a Decimal to any Base Algorithm
+
+    # Code Goes Here
+
+
+# 5.  Implement a program that coverts infix expressions to postfix expressions
+# 	→ "( A + B ) * ( C + D )"  ------- 'A B + C D + *'
+# 	→ ("( A + B ) * C"  -------- 'A B C * +'
+
+    # Code Goes Here
+
+
+# 6.  Implement a program that evaluates an expression already in its postfix
+# 	→ '7 8 + 3 2 + /' ------------ 3
+	
+    # Code Goes Here
+
+
+# 7. Implement an Algorithm that simulates a Hot Potato game
+# 	→ must use a queue
+
+    # Code Goes Here
+
+
+# 8.  Implement a Palindrome-Checker
+# 	→ use a deque
+
+    # Code Goes Here
+
+
+# 9. Implement a Complete Unordered List Class
+# 	→ implement a node class
+# 	→ must have the following methods
+# 		→ isEmpty() / add() / size() / search() / remove()
+
+    # Code Goes Here
+
+
+# 10.  Implement an Ordered List Class
+
+    # Code Goes Here
+
+
+# 11.   What is the big O of methods in linked lists
+
+    # Code Goes Here
+
