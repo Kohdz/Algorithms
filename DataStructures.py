b5104cbc4a595656a64b4a63d9497cc8b62f90db
kohdz <umair.stem@gmail.com>
2019-10-10 16:55:34 -0400

    Binary Tree Data Structure Added

--- DataStructures.py
+++ DataStructures.py
@@ -1,4 +1,4 @@
-class Stack():
+class Stack:
     def __init__(self):
         self.items = []
 
@@ -17,8 +17,7 @@ class Stack():
     def size(self):
         return len(self.items)
 
-
-class Queue():
+class Queue:
     def __init__(self):
         self.items = []
     
@@ -35,7 +34,7 @@ class Queue():
         return len(self.items)
 
 
-class Dequeue():
+class Dequeue:
     def __init__(self):
         self.items = []
 
@@ -57,7 +56,7 @@ class Dequeue():
     def size(self):
         return len(self.items)
 
-class Node():
+class Node:
     def __init__(self, initdata):
         self.data = initdata
         self.next = None
@@ -74,7 +73,7 @@ class Node():
     def setNext(self, nextdata):
         self.next = nextdata
 
-class UnorderedList():
+class UnorderedList:
     def __init__(self):
         self.head = None
 
@@ -105,8 +104,7 @@ class UnorderedList():
 
         return found
 
-    
-    def remove(self, item):
+        def remove(self, item):
         current = self.head
         previous = None
         found = False
@@ -119,8 +117,151 @@ class UnorderedList():
                 current = current.getData()
         
         if previous == None:
-            current.get
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getData())
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+    
+    def search(self, item):
+        current = self.head
+        found = False
+        stop = False
+
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+
+        return found
+
+    def add(self, item):
+        current = self.head
+        previous = Nne
+        stop = False
+
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+        
+    def isEmpty(self):
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+        def put(self, key, data):
+            hashvalue = self.heashfunction(key, len(self.slots))
+
+            if self.slots[hashvalue] == None:
+                self.slots[hashvalue] = key
+                self.data[hashvalue] = data
+            else:
+                if self.slots[hashvalue] == key:
+                    self.data[hashvalue] = data
+                else:
+                    nextslot = self.rehash(hashvalue, len(self.slots))
+                    while self.slots[nextslot] != None and \
+                                            self.slots[nextslot] != key:
+                        nextslot  self.rehash(nextslot, len(self.slots))
+
+                    if self.slots[nextslot] == None:
+                        self.slots[nextslot] = key
+                        self.data[nextslot] = data
+                    else:
+                        self.data[nextslot] = data
+
+        def hashfunction(self, key, size):
+            return key % size
+        
+        def rehash(self, oldhash, size):
+            return (oldhash + 1) % size
+
+        def get(self, key):
+            startslot = self.hashfunction(key, len(self.slots))
+
+            data = None
+            stop = False
+            found = False
+            position = startslot
+            while self.slots[position] != None and \
+                                not found and not stop:
+                if self.slots[position] == key:
+                    found = True
+                    data = self.data[position]
+                else:
+                    position = self.rehash(position, len(self.slots))
+                    if position == startslot:
+                        stop = True
+            
+            return data
+
+        def __getitem__(self, key):
+            return self.get(key)
+
+        def __setitem__(self, key, data):
+            self.put(key, data)
 
 
+class BinaryTree:
+    def __init__(self, rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
 
-class OrderedList()
+    def insertLeft(self, newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+    
+    def insertRight(self, newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+    def getRightChild(Self):
+        return self.rightChild
+
+    def gerLeftChild(self):
+        return self.leftChild
+    
+    def setRootVal(self, obj):
+        self.key = obj
+
+    def getRootVal(self):
+        return self.key
