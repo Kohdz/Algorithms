f999b02b6b766fb7530c8c6f51bc145472bf3167
kohdz <umair.stem@gmail.com>
2019-10-14 11:16:27 -0400

    AVL Tree

---
+++ LeetCode/easy/04_Roman_To_Int.py
@@ -0,0 +1,67 @@
+# Roman Numerals are represented by seven different symbols: I, V, X, L, D, D and M
+
+# I = 1
+# V = 5
+# X = 10
+# L = 50
+# C = 100
+# D = 500
+# M = 1000
+
+# Roman numerals are usally written largest to smallest from left to right.  Howerver, the numeral for four is not IIII
+# 4 = IV - because the one is before the five we subtract it making four
+# the same principle applies to the number nine, which is written as IX. There are six instance where subtraction is used:
+
+#     I can be placed before V(5) and x (10) to make 4 and 9
+#     X can be placed before L(50) and c (100) to make 40 and 90
+#     c can be replaced before L(50) and c (100) to make 40 and 90
+
+# Given  a roman numeral, convert it to an integer. Input is guaranteed to be withing the range from 1 to 3999
+
+# Example:
+#     Input: "III"
+#     Output: 3
+
+#     Input "IX"
+#     Output: 9
+
+#     Input "MCMXCIV"
+#     Output 1994
+#     Explanation: M = 1000 CM = 900, XC = 90 and IV = 4
+
+
+
+
+def romanInt(sym):
+    dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    counter = 0
+
+
+    t = [char for char in sym]
+    print(t)
+    index = 0
+    for i in t:
+        if i in dict:
+            if i == "I" and t[index] == 'V' or 'X':
+                counter += dict[i] - dict["I"]
+            elif i == "X" and t[index] == "L" or "C":
+                counter += dict[i] - dict["X"]
+            elif i == "C" and t[index] == "L" or "C":      
+                counter +- dict[i] - dict["C"]
+        else:
+            counter += dict[i]
+        index += 1
+
+    
+    return counter
+
+sym = "III"
+print(romanInt(sym))
--- Tester.py
+++ Tester.py
@@ -1,35 +1,30 @@
-# Given an array of integers, return indices of the two numbers such that they add up to a specific target
-# you may assume that each input would have exactly one solutuion, and you may not use the same element twice
-# Example:
+# Determine whether an integer is a palindrome. An integer is a palindrome when
+# it reads the same backwords as fordwards
 
-#     Given Nums = [2, 7, 11, 15], target = 9,
-#     because numbs[0] + nums[1] = 2 + 7 = 9,
-#     return [0, 1]
+# example 1:
+#     Input: 121
+#     Output: True
 
-
-nums = [2, 7, 11, 15]
-target = 9
-
-dwf twoSum(nums, target):
-    dict = {}
-    for i in range(len(nums)):
-        num2 = target = i
-        if num2 not in dict:
-            dict[target- num[i]] = i
-        else:
-            return [dict[num[i]], i]
-
-def toSum2(nums, target):
-    dict = {}
-    for i in range(len(nums)):
-        if target - nums[i] not in dict:
-            print("dict: ", dict)
-            dict[nums[i]] = i 
-            print("dict: ", dict)
-        else:
-            return [dict[target-nums[i]], i]
-
-print(toSumHerv2(nums, target))
+# example 2:
+#     Input: -121
+#     Output: False
 
 
+def isPal(num):
 
+    a = abs(num)
+    temp = 0
+    while a > 0:
+        mod = a % 10
+
+        temp = temp* 10  + mod
+        a = a // 10
+
+    if
+
+
+    return temp
+
+
+num = 121
+print(isPal(num))
