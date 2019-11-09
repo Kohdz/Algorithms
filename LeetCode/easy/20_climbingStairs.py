e76d3942b12bca3e2b8ff0e83ed04b4246b4823b
Umair Shabbir <umair.stem@gmail.com>
2019-11-09 17:40:59 -0500

    climbing Stairs

--- LeetCode/easy/20_climbingStairs.py
+++ LeetCode/easy/20_climbingStairs.py
@@ -57,3 +57,11 @@ print(climbStairs(n))
 #         nums[i] = nums[i-1] nums[i-2]
 
 #     return nums[n]
+
+
+def climbingStairs(n):
+
+    prev, current = 0, 1
+    for i in range(n):
+        prev, current = current, prev + current
+    return current
