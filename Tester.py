18383b3392c08ac46b334a13bed961c34f260029
Umair Shabbir <umair.stem@gmail.com>
2019-11-15 10:49:22 -0500

    Excel Sheet Title

--- Tester.py
+++ Tester.py
@@ -1,15 +1,10 @@
-# Write a Python program to calculate the sum of a list of numbers
 
 
-# Write a Python program to converting an Integer to a string in any base
+x, y = divmod(10, 3)
+print(x)
+print(y)
 
-
-def to_string(num, base):
-    conver_tString = "0123456789ABCDEF"
-    if num < base:
-        return conver_tString[num]
-    else:
-        return to_string(num//base, base) + conver_tString[num % base]
-
-
-print(to_string(2835, 16))
+x = 10 // 3
+y = 10 % 3
+print(x)
+print(y)
