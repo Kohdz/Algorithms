6a1cdadbb05e1ec09de371e39b88c1cc599a11f5
Umair Shabbir <umair.stem@gmail.com>
2019-11-15 16:34:25 -0500

    Elements Update

---
+++ Elements/Types/08_computeX^Y.py
@@ -0,0 +1,12 @@
+def power(x, y):
+    result, power = 1.0, y
+    if y < 0:
+        power, x = -power, 1.0 / x
+
+    while power:
+        if power & 1:
+            result *= x
+
+        x, power = x * x, power >> 1
+
+    return result
