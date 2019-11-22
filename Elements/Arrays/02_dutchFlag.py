c3d5c284e5b13a01aada680f03cbf9eb3d3447e2
Umair Shabbir <umair.stem@gmail.com>
2019-11-22 17:38:21 -0500

    The Dutch Flag

--- Elements/Arrays/02_dutchFlag.py
+++ Elements/Arrays/02_dutchFlag.py
@@ -48,6 +48,29 @@ def dutch_flag_partitionII(pivot_index, A):
     return A
 
 
+def dutch_flag_partitionIII(pivot_index, A):
+    pivot = A[pivot_index]
+
+    # keep the following invariants during paritioning:
+    # bottom group: A[:smaller]
+    # middle group: A[smaller:equal]
+    # unclassified group: A[equal:larger]
+    # top group: A[larger]
+
+    smaller, equal, larger = 0, 0, len(A)
+    while equal < larger:
+
+        # A[equal] is the incoming unclassified element
+        if A[equal] < pivot:
+            A[smaller], A[equal] = A[equal], A[smaller]
+            smaller, equal = smaller + 1, equal + 1
+        elif A[equal] == pivot:
+            equal += 1
+        else:  # A[equal] > pivot
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
+
+
 pivot_index = 3
 A = [0, 1, 2, 0, 2, 1]
 
