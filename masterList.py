56b667decae081cc43e8e8f8ce280581699a2a3a
Umair Shabbir <umair.stem@gmail.com>
2019-11-27 10:44:28 -0500

    Master List

---
+++ masterList.py
@@ -0,0 +1,54 @@
+# Linked Lists:
+#     - Implement A Linked List
+#     - Reverse A Linked List
+#     - Flatten a Nested Linked List
+#     - Add One
+#     - Duplicate Number
+#     - Max Sum Subarray
+#     - Pascal's Triangle
+#     - Even After Odd
+#     - Skip i, delete j
+#     - Swap Nodes
+
+# Stacks and Queues
+#     - Implement a Stack Using an Array
+#     - Implement a Stack Using a Linked list
+#     - Build a Stack
+#     - Balanced Parentheses
+#     - Reverse Polish Notation
+#     - Reverse a Stack
+#     - Minimum Bracket Reversals
+#     - Build a Queue using a List
+#     - Build a Queue using a Linked List
+#     - Build a Queue using a Stack
+#     - Build a Queue using High-Level Python
+#     - Reverse a Queue
+
+# Recursion
+#     - Factorial Function
+#     - Reverse a String
+#     - Palindrome Checking
+#     - List Permutations
+#     - Keypad Combinations
+#     - Deep Reverse
+#     - The Call Stack and Recursion
+#     - Recurrence Realations
+#     - Tower of Hanoi
+#     - Return Codes
+#     - Return Subsets
+#     - Staircase
+
+# Tree
+#     - Traversals (pre-in-post)
+#     - Depth First Search
+#         - Search and Delete
+#         - Insert
+#     - Binary Search Trees
+#     - Diameter of a Binary Tree
+#     - Path from Root to Node
+
+# Maps and Hashing
+#     - Implement a Hash Map
+#     - Implement Caching
+#     - Pair Sum to Target
+#     - Longest Consecutive Sequences
