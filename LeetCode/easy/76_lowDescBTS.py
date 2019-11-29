c0d6d4b44489c60bb7712bd22ecce46a1cbce057
Umair Shabbir <umair.stem@gmail.com>
2019-11-29 17:35:29 -0500

    Lowest Common Ancestor

--- LeetCode/easy/76_lowDescBTS.py
+++ LeetCode/easy/76_lowDescBTS.py
@@ -21,7 +21,6 @@
 # Explanation: The LCA of nodes 2 and 4 is 2, since
 # a node can be a descendant of itself according to the LCA definition.
 
-
 # Note:
 # All of the nodes' values will be unique.
 # p and q are different and both values will exist in the BST.
