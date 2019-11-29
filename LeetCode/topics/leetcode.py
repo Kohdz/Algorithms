4586c1dbe2b764a655a4a3ae87453a1e8e22be38
Umair Shabbir <umair.stem@gmail.com>
2019-11-29 17:34:50 -0500

    Lowest Common Acestor

--- LeetCode/topics/leetcode.py
+++ LeetCode/topics/leetcode.py
@@ -1,39 +1,39 @@
 # Array
-# twosum                          - https://leetcode.com/problems/two-sum/
-# Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-# Contains Duplicate              - https://leetcode.com/problems/contains-duplicate/
-# Maximum Subarray                - https://leetcode.com/problems/maximum-subarray/
+# twosum                           - https://leetcode.com/problems/two-sum/
+# Best Time to Buy and Sell Stock  - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# Contains Duplicate               - https://leetcode.com/problems/contains-duplicate/
+# Maximum Subarray                 - https://leetcode.com/problems/maximum-subarray/
 
 # Binary
-# Sum of Two Integers             - https://leetcode.com/problems/sum-of-two-integers/
-# Number of 1 Bits                - https://leetcode.com/problems/number-of-1-bits/
-# Missing Number                  - https://leetcode.com/problems/missing-number/
-# Reverse Bits                    - https://leetcode.com/problems/reverse-bits/
+# Sum of Two Integers              - https://leetcode.com/problems/sum-of-two-integers/
+# Number of 1 Bits                 - https://leetcode.com/problems/number-of-1-bits/
+# Missing Number                   - https://leetcode.com/problems/missing-number/
+# Reverse Bits                     - https://leetcode.com/problems/reverse-bits/
 
 
 # Dynamic Programing
-# Climbing Stairs                 - https://leetcode.com/problems/climbing-stairs/
-# House Robber                    - https://leetcode.com/problems/house-robber/
+# Climbing Stairs                  - https://leetcode.com/problems/climbing-stairs/
+# House Robber                     - https://leetcode.com/problems/house-robber/
 
 # Linked List
-# Reverse Linked List             -   https://leetcode.com/problems/reverse-linked-list/
-# Linked List Cycle               - https://leetcode.com/problems/linked-list-cycle/
-# Merge Two Sorted Lists          - https://leetcode.com/problems/merge-two-sorted-lists/
+# Reverse Linked List              - https://leetcode.com/problems/reverse-linked-list/
+# Linked List Cycle                - https://leetcode.com/problems/linked-list-cycle/
+# Merge Two Sorted Lists           - https://leetcode.com/problems/merge-two-sorted-lists/
 
 # Strings
-# Valid Anagram                   - https://leetcode.com/problems/valid-anagram/
-# Valid Parentheses               - https://leetcode.com/problems/valid-parentheses/
-# Valid Palindrome                - https://leetcode.com/problems/valid-palindrome/
+# Valid Anagram                    - https://leetcode.com/problems/valid-anagram/
+# Valid Parentheses                - https://leetcode.com/problems/valid-parentheses/
+# Valid Palindrome                 - https://leetcode.com/problems/valid-palindrome/
 
 # Trees
-# Maximum Dept of Binary Tree (26)- https://leetcode.com/problems/maximum-depth-of-binary-tree/
+# Maximum Dept of Binary Tree (26) - https://leetcode.com/problems/maximum-depth-of-binary-tree/
 # Same Tree (24)                   - https://leetcode.com/problems/same-tree/
 # Invert Binary Tree (47)          - https://leetcode.com/problems/invert-binary-tree/
-# Subtree of Another Tree         - https://leetcode.com/problems/subtree-of-another-tree/
-# Lowest Common Ancestor of BST   - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
-# Symmetric Tree                  - https://leetcode.com/problems/symmetric-tree/
-# Binary Tree Level Order Tra     - vhttps://leetcode.com/problems/binary-tree-level-order-traversal-ii/
-# Invert Binary Tree              - https://leetcode.com/problems/invert-binary-tree/
-# Sum of left leaves              - https://leetcode.com/problems/sum-of-left-leaves/
+# Subtree of Another Tree (75)     - https://leetcode.com/problems/subtree-of-another-tree/
+# Lowest Common Ancestor of BST    - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+# Symmetric Tree                   - https://leetcode.com/problems/symmetric-tree/
+# Binary Tree Level Order Tra      - vhttps://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+# Invert Binary Tree               - https://leetcode.com/problems/invert-binary-tree/
+# Sum of left leaves               - https://leetcode.com/problems/sum-of-left-leaves/
 
 # TreesII
