94ffe0058e7c159030edf655517a59188c28c68e
Umair Shabbir <umair.stem@gmail.com>
2019-11-30 12:46:41 -0500

    Birany Search Tree

--- DataStructures/Trees/BST.py
+++ DataStructures/Trees/BST.py
@@ -1,24 +1,50 @@
+from collections import deque
 
-# Build A Node
+
+class Queue():
+    def __init__(self):
+        self.q = deque()
+
+    def enq(self, value):
+        self.q.appendleft(value)
+
+    def deq(self):
+        if len(self.q) > 0:
+            return self.q.pop()
+        else:
+            return None
+
+    def __len__(self):
+        return len(self.q)
+
+    def __repr__(self):
+        if len(self.q) > 0:
+            s = "<enqueue here>\n_________________\n"
+            s += "\n_________________\n".join([str(item) for item in self.q])
+            s += "\n_________________\n<dequeue here>"
+            return s
+        else:
+            return "<queue is empty>"
 
 
-class Node:
+class Node(object):
+
     def __init__(self, value=None):
         self.value = value
         self.left = None
-        self. right = None
-
-    def get_value(self):
-        return self.value
+        self.right = None
 
     def set_value(self, value):
         self.value = value
 
-    def set_left_child(self, node):
-        self.left = node
+    def get_value(self):
+        return self.value
 
-    def set_right_child(self, node):
-        self.right = node
+    def set_left_child(self, left):
+        self.left = left
+
+    def set_right_child(self, right):
+        self.right = right
 
     def get_left_child(self):
         return self.left
@@ -30,66 +56,196 @@ class Node:
         return self.left != None
 
     def has_right_child(self):
-        return self.left != None
+        return self.right != None
 
-# create an instance of Node()
-# node0 = Node()
-# print(f"""
-# Node: node0
-# value: {node0.value}
-# left: {node0.left}
-# right: {node0.right}
+    # define __repr_ to decide what a print statement displays for a Node object
+    def __repr__(self):
+        return f"Node({self.get_value()})"
 
-# """)
-
-# node1 = Node("apple")
-# print(f"""
-# Node: node1
-# value: {node1.value}
-# left: {node1.left}
-# right: {node1.right}
-
-# """)
+    def __str__(self):
+        return f"Node({self.get_value()})"
 
 
-node0 = Node("apple")
-node1 = Node("bannana")
-node2 = Node("orange")
+class Tree():
+    def __init__(self):
+        self.root = None
 
-# node0.set_left_child(node1)
-# node0.set_right_child(node2)
-
-# print(f"""
-# node 0: {node0.value}
-# node 0 left.child: {node0.left.value}
-# node 0 right child: {node0.right.value}
-# """)
-
-print(f"had left child? {node0.has_left_child()}")
-print(f"had right child? {node0.has_right_child()}")
-
-print("adding left and right children")
-node0.set_left_child(node1)
-node0.set_right_child(node2)
-
-print(f"has left child? {node0.has_left_child()}")
-print(f"has right child? {node0.has_right_child()}")
-
-
-# define a tree class
-# tree is a collection of nodes
-# tree just needs to point to one node, the root
-class Tree(object):
-
-    # this method not perferred
-    # because user may not know
-    # create a node class
-
-    # def __init__(self, node ):
-    #     self.root = node
-
-    def __init__(self, value):
+    def set_root(self, value):
         self.root = Node(value)
 
     def get_root(self):
         return self.root
+
+    def compare(self, node, new_node):
+        """
+        0 means new_node equals node
+        -1 means new node less than existing node
+        1 means new node greater than existing node
+        """
+
+        if new_node.get_value() == node.get_value():
+            return 0
+        elif new_node.get_value() < node.get_value():
+            return -1  # traverse left
+        else:
+            return 1  # traverse right
+
+    def insert_with_loop(self, new_value):
+        new_node = Node(new_value)
+        node = self.get_root()
+        if node == None:
+            self.root = new_node
+            return
+
+        while(True):
+            comparison = self.compare(node, new_node)
+            if comparison == 0:
+                # override with new node's value
+                node.set_value(new_node.get_value())
+                break  # override node, and stop looping
+            elif comparison == -1:
+                # go left
+                if node.has_left_child():
+                    node = node.get_left_child()
+                else:
+                    node.set_left_child(new_node)
+                    break  # inserted node, so stop looping
+            else:  # comparison == 1
+                # go right
+                if node.has_right_child():
+                    node = node.get_right_child()
+                else:  # comparsion == 1
+                    # go right
+                    if node.has_right_child():
+                        node = node.get_right_child()
+                    else:
+                        node.set_right_child(new_node)
+                        break  # inserted node, so stop looping
+
+    def insert_with_recursion(self, value):
+
+        if self.get_root() == None:
+            self.set_root(value)
+            return
+        # otherwise use recursion to insert the node
+        self.insert_recursively(self.get_root(), Node(value))
+
+    def insert_recursively(self, node, new_node):
+        comparsion = self.compare(node, new_node)
+
+        if comparsion == 0:
+            # equal
+            node.set_value(new_node.get_value())
+        elif comparsion == -1:
+            # traverse left
+            if node.has_left_child():
+                self.insert_recursively(node.get_left_child(), new_node)
+            else:
+                node.set_left_child(new_node)
+
+        else:  # comparison == 1
+            # traverse right
+            if node.has_right_child():
+                self.insert_recursively(node.get_right_child(), new_node)
+            else:
+                node.set_right_child(new_node)
+
+    def search(self, value):
+        node = self.get_root()
+        s_node = Node(value)
+        while(True):
+            comparsion = self.compare(node, s_node)
+            if comparsion == 0:
+                return True
+            elif comparsion == -1:
+                if node.has_left_child():
+                    node = node.get_left_child()
+                else:
+                    return False
+            else:
+                if node.has_right_child():
+                    node = node.get_right_child()
+                else:
+                    return False
+
+    def __repr__(self):
+            # initialization
+        level = 0
+        q = Queue()
+        visit_order = list()
+        node = self.get_root()
+        q.enq((node, level))  # enque a tuple containing node and its level
+        ###
+
+        while(len(q) > 0):
+
+            node, level = q.deq()
+            # vinland(
+            if node == None:
+                visit_order.append(("<empty>", level))
+                continue
+
+            visit_order.append((node, level))
+
+            if node.has_left_child():
+                q.enq((node.get_left_child(), level + 1))
+            else:
+                q.enq((None, level + 1))
+
+            if node.has_right_child():
+                q.enq((node.get_right_child(), level + 1))
+            else:
+                q.enq((None, level + 1))
+
+        # write a delete function
+        s = "Tree\n"
+        previous_level = -1
+        for i in range(len(visit_order)):
+            node, level = visit_order[i]
+            # At first previous_level not the ame as level so you
+            # Start prining with a newline in the else statement
+            # then previous level gets incremented and the next time
+            # around previous level will be same as level for all elements in the same level
+            # until we hit a new level where level > previous level and we print a new line
+            # and Repeat!
+
+            if level == previous_level:
+                s += " | " + str(node)
+            else:
+                s += "\n" + str(node)
+                previous_level = level
+
+        return s
+
+
+# loopin
+# tree = Tree()
+# tree.insert_with_loop(5)
+# tree.insert_with_loop(6)
+# tree.insert_with_loop(4)
+# tree.insert_with_loop(2)
+# tree.insert_with_loop(5)  # insert duplicate
+# print(tree)
+
+
+# recursion
+# tree = Tree()
+# tree.insert_with_recursion(5)
+# tree.insert_with_recursion(6)
+# tree.insert_with_recursion(4)
+# tree.insert_with_recursion(2)
+# tree.insert_with_recursion(5)  # insert duplicate
+# print(tree)
+
+# insert
+tree = Tree()
+tree.insert_with_recursion(5)
+tree.insert_with_recursion(6)
+tree.insert_with_recursion(4)
+tree.insert_with_recursion(2)
+
+print(f"""
+search for 8: {tree.search(8)}
+search for 2: {tree.search(2)}
+
+""")
