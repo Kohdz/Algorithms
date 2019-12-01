7fda9520df9acac34c0421af1e9d260b490aa23b
Umair Shabbir <umair.stem@gmail.com>
2019-12-01 10:54:41 -0500

    Symetric Tree

--- LeetCode/easy/25_symTree.py
+++ LeetCode/easy/25_symTree.py
@@ -19,7 +19,6 @@
 #    \   \
 #    3    3
 
-
 def isSymmetric(root):
 
     if root is None:
@@ -37,3 +36,25 @@ def isSymetricRec(left, right):
     return isSymetricRec(left.left, right.right) and isSymetricRec(left.right, right.left)
 
 # space complexity is poroptial to the height of the tree and scales with ith
+
+
+# Iterative Python BFS Solution
+def isSymetricBFS(root):
+
+    if not root:
+        return True
+
+    queue = [root]
+    while queue:
+        value = [x.val if x else None for x in queue]
+
+        if value[::-1] != value:
+            return False
+
+        new_queue = []
+        for node in queue:
+            if node:
+                new_queue.append(node.left)
+                new_queue.append(node.right)
+            queue = new_queue
+        return True
