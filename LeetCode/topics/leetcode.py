e66f1bcd94b8dc47fe14cdf15761cafb36182d93
Umair Shabbir <umair.stem@gmail.com>
2019-11-30 22:00:42 -0500

    tree questions updated

--- LeetCode/topics/leetcode.py
+++ LeetCode/topics/leetcode.py
@@ -35,5 +35,38 @@
 # Binary Tree Level Order Tra      - vhttps://leetcode.com/problems/binary-tree-level-order-traversal-ii/
 # Invert Binary Tree               - https://leetcode.com/problems/invert-binary-tree/
 # Sum of left leaves               - https://leetcode.com/problems/sum-of-left-leaves/
+# Diameter of a Binary Tree        - https://leetcode.com/problems/diameter-of-binary-tree/
+# Binary Tree Paths                - https://leetcode.com/problems/binary-tree-paths/
 
 # TreesII
+# Range Sum of BST                 - https://leetcode.com/problems/range-sum-of-bst/
+# Merge Two Binary Trees           - https://leetcode.com/problems/range-sum-of-bst/
+# Search in a BST                  - https://leetcode.com/problems/search-in-a-binary-search-tree/
+#  N-ary Tree PostOrder Traversal  - https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+#  N-ary Tree PreOrder Traversal   - https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+#  Univalued Binary                - Tree https://leetcode.com/problems/univalued-binary-tree/
+# Max Depth of N-ary Tree          - https://leetcode.com/problems/univalued-binary-tree/
+# Increasing Order Search Tree     - https://leetcode.com/problems/increasing-order-search-tree/
+# Leaf-Similar Trees               - https://leetcode.com/problems/leaf-similar-trees/
+# Sum of Root To Leaf Binary Nums  - https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+# Trim a BST                       - https://leetcode.com/problems/trim-a-binary-search-tree/
+# Convert Sorted Array to BST      - https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+# Two Sum IV - Input is a BST      - https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+# Convert BST to Greater Tree      - https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+# Construct String From Binary Tree - https://leetcode.com/problems/construct-string-from-binary-tree/
+# Cousins in Binary Tree            - https://leetcode.com/problems/cousins-in-binary-tree/
+# Minimum Absolute Difference in BST - https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+# Min Distance Between BTS Nodes     - https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+# Sum of Left Leaves                 - https://leetcode.com/problems/sum-of-left-leaves/
+# Binary Tree Level Order Traversal II - https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+# Binary Tree Paths                    - https://leetcode.com/problems/binary-tree-paths/
+# DIameter of Binary Tree              - https://leetcode.com/problems/diameter-of-binary-tree/
+# Binary Tree Tilt                     - https://leetcode.com/problems/binary-tree-tilt/
+# Path Sum III                         - https://leetcode.com/problems/path-sum-iii/
+# Subtree of Another Tree              - https://leetcode.com/problems/subtree-of-another-tree/
+# Second Minimum Node in a BT          - https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
+# Balanced Binary Tree                 - https://leetcode.com/problems/balanced-binary-tree/
+# Find Mode in Binary Search Tree      - https://leetcode.com/problems/find-mode-in-binary-search-tree/
+# Path Sum                             - https://leetcode.com/problems/path-sum/
+# Minimum Depth of Binary Tree         - https://leetcode.com/problems/minimum-depth-of-binary-tree/
+# Longest Univalue Path                - https://leetcode.com/problems/longest-univalue-path/
