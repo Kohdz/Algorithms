fff0223980617f2b2023283a7e8b7f463b3548ad
Umair Shabbir <umair.stem@gmail.com>
2019-12-25 11:03:51 -0500

    computeXY

--- Elements/Types/06_computeXY.py
+++ Elements/Types/06_computeXY.py
@@ -1,23 +1,56 @@
 def multiply(x, y):
     def add(a, b):
+        print("a: ", bin(a))
+        print("b: ", bin(b))
         running_sum, carryin, k, temp_a, temp_b = 0, 0, 1, a, b
-
         while temp_a or temp_b:
+            print("k: ", bin(k))
+            print("carryin: ", bin(carryin))
+            print("temp_a: ", bin(temp_a))
+            print("tempb: ", bin(temp_b))
+            print("")
             ak, bk = a & k, b & k
+            print("ak: ", bin(ak))
+            print("bk: ", bin(bk))
+            print("ak & k: ", bin(ak & k))
+            print("bk & k: ", bin(bk & k))
+            print("")
+            print("ak & bk: ", bin(ak & bk))
+            print("ak & carryin: ", bin(ak & carryin))
+            print("bk & carryin: ", bin(bk & carryin))
+            print("")
             carryout = (ak & bk) | (ak & carryin) | (bk & carryin)
+            print("ak ^ bk: ", bin(ak ^ bk))
+            print("carryin: ", bin(carryin))
             running_sum |= ak ^ bk ^ carryin
-            carryin, k, temp_b, temp_b = (
-                carryout << 1, k << 1, temp_a >> 1, temp_b >> 1)
+            print("running_sum: ", bin(running_sum))
+            print("")
 
+            carryin, k, temp_a, temp_b = \
+                (carryout << 1, k << 1, temp_a >> 1, temp_b >> 1)
+            print("carryout: ", bin(carryout))
+            print("k: ", bin(k))
+            print("temp_a: ", bin(temp_a))
+            print("temp_b: ", bin(temp_b))
+            print("")
+
+        print("running_sum | carrying: ", bin(running_sum | carryin))
         return running_sum | carryin
 
     running_sum = 0
-    while x:  # Examines each bit of x
+    while x:  # examines each bit of x
+        print("x: ", bin(x))
+        print("")
         if x & 1:
+            print("x & 1: ", bin(x & 1))
             running_sum = add(running_sum, y)
+        print("x: ", bin(x >> 1))
+        print("y: ", bin(y << 1))
+        print("")
         x, y = x >> 1, y << 1
-
     return running_sum
 
 
-print(multiply(2, 2))
+x = 3
+y = 2
+print(multiply(x, y))
--- tester2.py
+++ tester2.py
@@ -1,31 +1,56 @@
+def multiply(x, y):
+    def add(a, b):
+        print("a: ", bin(a))
+        print("b: ", bin(b))
+        running_sum, carryin, k, temp_a, temp_b = 0, 0, 1, a, b
+        while temp_a or temp_b:
+            print("k: ", bin(k))
+            print("carryin: ", bin(carryin))
+            print("temp_a: ", bin(temp_a))
+            print("tempb: ", bin(temp_b))
+            print("")
+            ak, bk = a & k, b & k
+            print("ak: ", bin(ak))
+            print("bk: ", bin(bk))
+            print("ak & k: ", bin(ak & k))
+            print("bk & k: ", bin(bk & k))
+            print("")
+            print("ak & bk: ", bin(ak & bk))
+            print("ak & carryin: ", bin(ak & carryin))
+            print("bk & carryin: ", bin(bk & carryin))
+            print("")
+            carryout = (ak & bk) | (ak & carryin) | (bk & carryin)
+            print("ak ^ bk: ", bin(ak ^ bk))
+            print("carryin: ", bin(carryin))
+            running_sum |= ak ^ bk ^ carryin
+            print("running_sum: ", bin(running_sum))
+            print("")
+
+            carryin, k, temp_a, temp_b = \
+                (carryout << 1, k << 1, temp_a >> 1, temp_b >> 1)
+            print("carryout: ", bin(carryout))
+            print("k: ", bin(k))
+            print("temp_a: ", bin(temp_a))
+            print("temp_b: ", bin(temp_b))
+            print("")
+
+        print("running_sum | carrying: ", bin(running_sum | carryin))
+        return running_sum | carryin
+
+    running_sum = 0
+    while x:  # examines each bit of x
+        print("x: ", bin(x))
+        print("")
+        if x & 1:
+            print("x & 1: ", bin(x & 1))
+            running_sum = add(running_sum, y)
+        print("x: ", bin(x >> 1))
+        print("y: ", bin(y << 1))
+        print("")
+        x, y = x >> 1, y << 1
+    return running_sum
 
 
-def plus_one(A):
-
-    # add 1 to last element
-    # loop through element end to front
-    # add one and change number to 0
-    # brake if not 10
-
-    # check if 1th element is 10
-    # and adujust
-
-    A[-1] += 1
-
-    for i in reversed(range(1, len(A))):
-
-        if A[i] != 10:
-            break
-
-        A[i] = 0
-        A[i-1] += 1
-
-    if A[0] == 10:
-        A[0] = 1
-        A.append(0)
-
-    return A
-
-
-A = [9, 9]
-print(plus_one(A))
+x = 3
+y = 2
+print(multiply(x, y))
