052d840fe8cf9691f1e09702ecf54ee3a62c31fc
Umair Shabbir <umair.stem@gmail.com>
2020-01-01 10:23:54 -0500

    isPalDecimal

--- tester2.py
+++ tester2.py
@@ -1,19 +1,27 @@
-def divide(x, y):
-
-        result, power = 0, 32
-        y_power = y << power
-
-        while x >= y:
-
-            while y_power > x:
-
-                y_power >>= 1
-                power -= 1
-
-            result += 1 << power
-            x -= y_power
-
-        return result
+import math
 
 
-print(divide(10, 3))
+def is_palindrome(x):
+
+    if x <= 0:
+        return x == 0
+
+    num_digits = math.floor(math.log10(x)) + 1
+    msd_mask = 10**(num_digits - 1)
+
+    # this gives us the number of digits that brilla
+    num_digits = math.floor(math.log10(x))
+
+    for i in range(num_digits // 2):
+
+        if x // msd_mask != x % 10:
+            return False
+
+        x %= msd_mask  # removes the most significant digit of x
+        x //= 10  # removes the least significant digit of x
+        msd_mask //= 100
+
+    return True
+
+
+print(is_palindrome(1221))
