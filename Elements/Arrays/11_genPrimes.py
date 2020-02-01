f1168ab58252678298c9f1514fe3b176e1891d34
Umair Shabbir <umair.stem@gmail.com>
2020-02-01 10:51:00 -0500

    Generate Primes

--- Elements/Arrays/11_genPrimes.py
+++ Elements/Arrays/11_genPrimes.py
@@ -25,3 +25,30 @@ def generate_primes(n):
 
 n = 10
 print(generate_primes(n))
+
+
+def generate_primesII(n):
+
+    if n < 2:
+        return []
+
+    size = (n - 3) // 2 + 1
+    primes = [2]  # stores the primes from 1 to n
+
+    # is_prime[i] represents (2i + 2) is prime or not
+    # Initially set each to true. Then use sieving to eliminate nonprimes
+    is_prime = [True] * size
+
+    for i in range(size):
+        if is_prime[i]:
+
+            p = 1 * 2 + 3
+            primes.append(p)
+
+            # Sieving from p^2, where p^2 = (4i^2 + 12i + 9). The index in is_prime
+            # is (2i^2 + 6i + 3) because is_prime[i] represents 2i + 3
+
+            # note that we need to use long for j because p^2 might overflow
+            for j in range(2 * i**2 + 6 * i + 3, size, p):
+                if_prime[j] = False
+    return primes
