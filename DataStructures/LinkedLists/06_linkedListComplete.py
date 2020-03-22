e5bd64fa3374c9793345c560bd5765b221ae8a30
Umair Shabbir <umair.stem@gmail.com>
2020-03-21 23:14:48 -0400

    Linked List Complete

--- DataStructures/LinkedLists/06_linkedListComplete.py
+++ DataStructures/LinkedLists/06_linkedListComplete.py
@@ -53,6 +53,7 @@ class LinkedList:
                     break
                 else:
                     position_prev_tail.next = position_tail.next
+
                     break
 
             position_prev_tail = position_tail
@@ -114,59 +115,68 @@ class LinkedList:
 # Test your implementation here
 
 
-# Test prepend
-linked_list = LinkedList()
-linked_list.prepend(1)
-assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
-linked_list.append(3)
-linked_list.prepend(2)
-assert linked_list.to_list() == [
-    2, 1, 3], f"list contents: {linked_list.to_list()}"
+class Test:
 
-# Test append
-linked_list = LinkedList()
-linked_list.append(1)
-assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
-linked_list.append(3)
-assert linked_list.to_list() == [
-    1, 3], f"list contents: {linked_list.to_list()}"
+    # Test prepend
+    linked_list = LinkedList()
+    linked_list.prepend(1)
+    assert linked_list.to_list() == [
+        1], f"list contents: {linked_list.to_list()}"
+    linked_list.append(3)
+    linked_list.prepend(2)
+    assert linked_list.to_list() == [
+        2, 1, 3], f"list contents: {linked_list.to_list()}"
 
-# Test search
-linked_list.prepend(2)
-linked_list.prepend(1)
-linked_list.append(4)
-linked_list.append(3)
-assert linked_list.search(
-    1).value == 1, f"list contents: {linked_list.to_list()}"
-assert linked_list.search(
-    4).value == 4, f"list contents: {linked_list.to_list()}"
+    # Test append
+    linked_list = LinkedList()
+    linked_list.append(1)
+    assert linked_list.to_list() == [
+        1], f"list contents: {linked_list.to_list()}"
+    linked_list.append(3)
+    assert linked_list.to_list() == [
+        1, 3], f"list contents: {linked_list.to_list()}"
 
-# Test remove
-linked_list.remove(1)
-assert linked_list.to_list() == [2, 1, 3, 4,
-                                 3], f"list contents: {linked_list.to_list()}"
-linked_list.remove(3)
-assert linked_list.to_list() == [
-    2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
-linked_list.remove(3)
-assert linked_list.to_list() == [
-    2, 1, 4], f"list contents: {linked_list.to_list()}"
+    # Test search
+    linked_list.prepend(2)
+    linked_list.prepend(1)
+    linked_list.append(4)
+    linked_list.append(3)
+    assert linked_list.search(
+        1).value == 1, f"list contents: {linked_list.to_list()}"
+    assert linked_list.search(
+        4).value == 4, f"list contents: {linked_list.to_list()}"
 
-# Test pop
-value = linked_list.pop()
-assert value == 2, f"list contents: {linked_list.to_list()}"
-assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
+    # Test remove
+    linked_list.remove(1)
+    assert linked_list.to_list() == [2, 1, 3, 4,
+                                     3], f"list contents: {linked_list.to_list()}"
+    linked_list.remove(3)
+    assert linked_list.to_list() == [
+        2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+    linked_list.remove(3)
+    assert linked_list.to_list() == [
+        2, 1, 4], f"list contents: {linked_list.to_list()}"
 
-# Test insert
-linked_list.insert(5, 0)
-assert linked_list.to_list() == [
-    5, 1, 4], f"list contents: {linked_list.to_list()}"
-linked_list.insert(2, 1)
-assert linked_list.to_list() == [
-    5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
-linked_list.insert(3, 6)
-assert linked_list.to_list() == [5, 2, 1, 4,
-                                 3], f"list contents: {linked_list.to_list()}"
+    # Test pop
+    value = linked_list.pop()
+    assert value == 2, f"list contents: {linked_list.to_list()}"
+    assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
 
-# Test size
-assert linked_list.size() == 5, f"list contents: {linked_list.to_list()}"
+    # Test insert
+    linked_list.insert(5, 0)
+    assert linked_list.to_list() == [
+        5, 1, 4], f"list contents: {linked_list.to_list()}"
+    linked_list.insert(2, 1)
+    assert linked_list.to_list() == [
+        5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
+    linked_list.insert(3, 6)
+    assert linked_list.to_list() == [5, 2, 1, 4,
+                                     3], f"list contents: {linked_list.to_list()}"
+
+    # Test size
+    assert linked_list.size() == 5, f"list contents: {linked_list.to_list()}"
+
+    print("Tests Run Without any Problem")
+
+
+Test()
