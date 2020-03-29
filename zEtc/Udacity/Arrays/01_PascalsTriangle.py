fd4f5e46033a72741df3824bba40c784ef6b3631
Umair Shabbir <umair.stem@gmail.com>
2020-03-28 21:10:07 -0400

    Pscals Triangle

---
+++ zEtc/Udacity/Arrays/01_PascalsTriangle.py
@@ -0,0 +1,39 @@
+
+# https://leetcode.com/problems/pascals-triangle/solution/
+
+# Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+
+# Input: 5
+# Output:
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+
+# Dynamic Programming Approach
+
+def generate(num_rows):
+
+    triangle = []
+
+    for row_num in range(num_rows):
+        # The first and last row elements are always 1
+        row = [None for _ in range(row_num + 1)]
+        row[0], row[-1] = 1, 1
+
+        # Each Triangle element is equal to the sum of the elements
+        # above-and-to-the-left and above-and-to-the-right
+        for j in range(1, len(row)-1):
+            row[j] = triangle[row_num-1][j - 1] + triangle[row_num-1][j]
+
+        triangle.append(row)
+
+    return triangle
+
+
+num_rows = 5
+print(generate(num_rows))
