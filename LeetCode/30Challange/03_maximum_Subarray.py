7193256fb7707f57c93048b0ab6c593706998c2f
Umair Shabbir <umair.stem@gmail.com>
2020-04-04 16:35:30 -0400

    Maximum Subarray

---
+++ LeetCode/30Challange/03_maximum_Subarray.py
@@ -0,0 +1,15 @@
+
+def maxSubArray(nums):
+    if max(nums) < 0:
+        return max(nums)
+
+    local_max, global_max = 0, 0
+    for num in nums:
+        local_max = max(0, local_max + num)
+        global_max = max(global_max, local_max)
+
+    return global_max
+
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxSubArray(nums))
