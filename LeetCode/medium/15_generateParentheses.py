d42e80232383b9608a89b531cbee98094dec2ed9
Umair Shabbir <umair.stem@gmail.com>
2020-04-14 22:09:52 -0400

    generate parenthesis

---
+++ LeetCode/medium/15_generateParentheses.py
@@ -0,0 +1,21 @@
+def generateParenthesis(N):
+
+    ans = []
+
+    def backtrack(S='', left=0, right=0):
+        if len(S) == 2 * N:
+            ans.append(S)
+            return
+
+        if left < N:
+            backtrack(S + "(", left+1, right)
+
+        if right < left:
+            backtrack(S+')', left, right + 1)
+
+    backtrack()
+    return ans
+
+
+n = 3
+print(generateParenthesis(n))
