afd2b1cefd2fc6050399abd09f588b5ab8fe3791
Umair Shabbir <umair.stem@gmail.com>
2020-05-03 14:50:09 -0400

    Max Subarray Update

--- LeetCode/AprilChallange/03_maximum_Subarray.py
+++ LeetCode/AprilChallange/03_maximum_Subarray.py
@@ -12,3 +12,15 @@ def maxSubArray(nums):
 
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 print(maxSubArray(nums))
+
+
+# def maxSubArrayII(A):
+#     if not A:
+#         return 0
+
+#     curSum = maxSum = A[0]
+#     for num in A[1:]:
+#         curSum = max(num, curSum + num)
+#         maxSum = max(maxSum, curSum)
+
+#     return maxSum
