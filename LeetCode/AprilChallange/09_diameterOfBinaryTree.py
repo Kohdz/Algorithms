f5c569b0e93c559dc9f15841a9d059fe53dbdf62
Umair Shabbir <umair.stem@gmail.com>
2020-05-05 13:21:28 -0400

    Diameter Of A Binary Tree

---
+++ LeetCode/AprilChallange/09_diameterOfBinaryTree.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def diameterOfBinaryTree(self, root):
+
+        self.ans = 1
+
+        def depth(node):
+            if not node:
+                return 0
+            L = depth(node.left)
+            R = depth(node.right)
+            self.ans = max(self.ans, L+R+1)
+            return max(L, R) + 1
+
+        depth(root)
+        return self.ans - 1
