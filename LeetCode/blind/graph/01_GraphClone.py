81add3786051faf74a5e0c52548467f3e6fc8e54
kohdz <umair.stem@gmail.com>
2020-05-29 13:17:40 -0400

    Graph Clone

---
+++ LeetCode/blind/graph/01_GraphClone.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/clone-graph/
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+def cloneGraph(node):
+    dic = {}
+
+    def dfs(node):
+        if not node:
+            return
+        else:
+            node_copy = Node(node.val, [])
+            dic[node] = node_copy
+            for nei in node.neighbors:
+                if nei in dic:
+                    node_copy.neighbors.append(dic[nei])
+                else:
+                    node_copy.neighbors.append(dfs(nei))
+            return node_copy
+    return dfs(node)
