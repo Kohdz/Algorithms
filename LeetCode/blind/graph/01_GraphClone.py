766bbec9f451c0416049c187335d67590f321858
kohdz <umair.stem@gmail.com>
2020-05-30 18:59:25 -0400

    Graph Datastructure

--- LeetCode/blind/graph/01_GraphClone.py
+++ LeetCode/blind/graph/01_GraphClone.py
@@ -1,14 +1,15 @@
 # https://leetcode.com/problems/clone-graph/
 
+import collections
 
-# Definition for a Node.
+
 class Node:
     def __init__(self, val=0, neighbors=None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
 
-def cloneGraph(node):
+def cloneGraphDFS(node):
     dic = {}
 
     def dfs(node):
@@ -24,3 +25,20 @@ def cloneGraph(node):
                     node_copy.neighbors.append(dfs(nei))
             return node_copy
     return dfs(node)
+
+
+def cloneGraphBFS(node):
+    if not node:
+        return node
+    q = collections.deque([node])
+    visited = {}
+    visited[node] = Node(node.val, [])
+
+    while q:
+        n = q.popleft()
+        for neighbor in n.neighbors:
+            if neighbor not in visited:
+                visited[neighbor] = Node(neighbor.val, [])
+                q.append(neighbor)
+            visited[n].neighbors.append(visited[neighbor])
+    return visited[node]
