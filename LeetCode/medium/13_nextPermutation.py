88ed19fd44f15b9a483bd23773cc38f7edba4804
unknown <umair.stem@gmail.com>
2020-06-20 21:14:55 -0400

    Next Permutation

--- LeetCode/medium/13_nextPermutation.py
+++ LeetCode/medium/13_nextPermutation.py
@@ -1,3 +1,41 @@
+# https://leetcode.com/problems/next-permutation
+
+
+def nextPermutationnums(nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+    #This is a math problem. From right to left, find the first one decreasing from the right. Num1
+    #Then start from the right again, to find the first one larger than Num1
+    #Swap Num1 and Num2, and then reverse all the following arrays after position of Num1
+    #Time complexity, O(n), two scan
+    #Spcae complexity, O(1), in place
+        
+        # Find the index where nums[i] < nums[i+1]
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        
+        # list is sorted in decending order, return revered
+        if i == 0:   # nums are in descending order
+            nums.reverse()
+            return 
+       
+        # find the index j where nums[j] is larger than nums[i] and swap
+        k = i - 1    # find the last "ascending" position
+        while nums[j] <= nums[k]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]  
+        
+        # sort the rest of the list in ascending order
+        l, r = k+1, len(nums)-1  # reverse the second part
+        
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l +=1 ; r -= 1
+
+# EPI
 def nextPermutation(perm):
     """
     Do not return anything, modify nums in-place instead.
