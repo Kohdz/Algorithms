6d403eeed021d78717e95b627db8d7b1674d819d
unknown <umair.stem@gmail.com>
2020-06-26 09:58:49 -0400

    Course Schedule

--- LeetCode/medium/11_CourseSchedule.py
+++ LeetCode/medium/11_CourseSchedule.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/course-schedule/
 # Topological Sort
 
+import collections
 
 
 def canFinish(numCourses, prerequisites):
@@ -41,3 +42,70 @@ prerequisites = [[1, 0], [0, 1]]
 # prerequisites2 = [[1,0]]
 
 print(canFinish(NumCourses, prerequisites))
+
+
+def canFinishIII(numCourses, prerequisites):
+        
+     # Constant defined for course state
+    NOT_CHECKED, CHECKING, COMPLETED = 0, 1, 2
+        
+    # -------------------------------
+        
+    def has_deadlock( course )->bool:
+            
+        if course_state[course] == CHECKING:
+            # There is a cycle(i.e., deadlock ) in prerequisites
+            return True
+            
+        elif course_state[course] == COMPLETED:
+            # current course has been checked and marked as completed
+             return False
+            
+            
+            
+        # update current course as checking
+        course_state[course] = CHECKING
+            
+        # check pre_course in DFS and detect whether there is deadlock
+        for pre_course in requirement[course]:
+                
+            if has_deadlock( pre_course ):
+                # deadlock is found, impossible to finish all courses
+                return True
+                
+                                
+        # update current course as completed
+        course_state[course] = COMPLETED
+            
+        return False
+        
+    # -------------------------------
+        
+    # each course maintain a list of its own prerequisites
+    requirement = collections.defaultdict( list )
+        
+    for course, pre_course in prerequisites:
+        requirement[course].append( pre_course )
+        
+        
+    # each course maintain a state among {NOT_CHECKED, CHECKING, COMPLETED}
+	# Initial state is NOT_CHECKED 
+    course_state = [ NOT_CHECKED for _ in range(numCourses) ]
+           
+    # Launch cycle (i.e., deadlock ) detection in DFS
+    for course_idx in range(0, numCourses):
+            
+        if has_deadlock(course_idx):
+            # deadlock is found, impossible to finish all courses
+            return False
+        
+    # we can finish all course with required order 
+    return True
+
+# NumCourses = 2
+# prerequisites = [[1, 0], [0, 1]]
+
+numCourses1 = 2
+prerequisites2 = [[1,0]]
+
+print(canFinishIII(numCourses1, prerequisites2))
