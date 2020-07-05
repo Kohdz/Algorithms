140c82715a019ba92b6114a31aead320b07f5ea8
unknown <umair.stem@gmail.com>
2020-07-05 10:56:59 -0400

    Flip Sign

---
+++ codeForces/flipSign.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+# flip sign for ai if its odd else vice vers
+
+
+
+
+def sign_flip(size, A):
+    pass
+
+
+
+
+
+size1 = 3
+A1 = [-2, 4, 3]
+
+size2 = 5
+A2 = [1, 1, 1, 1, 1]
+
+size3 = 5
+A3 = [-2, 4, 7, -6, 4]
+
+size4 = 9 
+A4 = [9, 7, -4, -2, 1, -3, 9, -4, -5]
+
+size5 = 9
+A5 = [-4, 1, 9, 4, 8, 9, 5, 1, -9]
+
+# print(sign_flip(size1, A1) == [-2, -4, 3])
+# print(sign_flip(size2, A2) == [1, 1, 1, 1, 1])
+# print(sign_flip(size3, A3) == [-2, -4, 7, -6, 4])
+# print(sign_flip(size4, A4) == [9, -7, -4, 2, 1, -3, -9, -4, -5])
+# print(sign_flip(size5, A5) == [4, -1, -9, -4, -8, -9, -5, -1, 9])
