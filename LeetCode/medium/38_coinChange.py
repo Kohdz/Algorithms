e4a8518c419a1f1ce6703e5e70b315a2ab439737
unknown <umair.stem@gmail.com>
2020-07-14 09:23:44 -0400

    Coin Change

---
+++ LeetCode/medium/38_coinChange.py
@@ -0,0 +1,60 @@
+# https://leetcode.com/problems/coin-change/
+
+
+# iterative bottom up
+def coinChange(coins, amount):
+    
+    dp = [float('inf')] * (amount + 1)
+    
+    dp[0] = 0
+    
+    for coin in coins:
+        for x in range(coin, amount + 1):
+            
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+    
+    return dp[amount] if dp[amount] != float('inf') else -1 
+
+
+
+# Input: coins = [1, 2, 5], amount = 11
+# Output: 3 
+# Explanation: 11 = 5 + 5 + 1
+
+amount = 11
+coins = [1, 2, 5]
+print(coinChange(coins, amount))
+
+
+# recursion/top down
+def coinChangeTopDown(coins, amount):
+
+    if amount < 1:
+        return 0
+    
+    dp = [-1] * (amount + 1)
+    
+    def find(curr_amount):
+        
+        if curr_amount == 0:
+            return 0
+        
+        if curr_amount < 0:
+            return float('inf')
+        
+        if dp[curr_amount] != -1:
+            return dp[curr_amount]
+        
+        min_ways = float('inf')
+        
+        for c in coins:
+            res = find(curr_amount - c) + 1
+            min_ways = min(min_ways, res)
+        
+        dp[curr_amount] = min_ways
+        
+        return dp[curr_amount]
+    
+    find(amount)
+    
+    return dp[-1] if dp[-1] != float('inf') else -1
