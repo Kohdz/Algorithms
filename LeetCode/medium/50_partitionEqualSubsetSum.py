a7cd64af05a740139ba5c21bb2eee92572a5b0b8
unknown <umair.stem@gmail.com>
2020-07-20 11:15:58 -0400

    Partition Equal Subset Sum

---
+++ LeetCode/medium/50_partitionEqualSubsetSum.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/partition-equal-subset-sum/
+# https://leetcode.com/problems/partition-equal-subset-sum/discuss/462699/Whiteboard-Editorial.-All-Approaches-explained.
+
+
+def canPartition(A):
+    
+    if not A:
+        return True
+
+    # in case total is odd, there is no way
+    # for us to evenly divide the sum
+    total = sum(A)
+    
+    if total & 1:
+        return False
+    
+    # divide total by 2?
+    total >>= 1
+    
+    dp = [False] * (total + 1)
+    dp[0] = True
+    
+    for i in range(1, len(A)):
+        
+        for j in range(total, A[i] - 1, -1):
+            
+            dp[j] = dp[j] or dp[j - A[i]]
+            
+    return dp[-1]
+
+
+A = [1, 5, 11, 5]
+# Output: true
+# Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+print(canPartition(A))
