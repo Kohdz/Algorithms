72b2157956ac08d6e2b6555fb1c006b5be2daa11
unknown <umair.stem@gmail.com>
2020-07-20 21:59:40 -0400

    Sqrtx

--- LeetCode/easy/19_sqrt.py
+++ LeetCode/easy/19_sqrt.py
@@ -37,19 +37,19 @@ x = 8
 
 
 def sqrtHer(x):
-    #  if x = 0, sqrt of 0 is 0
-    # if x = 1, sqrt of 1 is 1
-    if x < 2:
-        return x
-
-    left, right = 1,  x//2
+    left, right = 0, x
+    
     while left <= right:
-        mid = left + (right-left) // 2
-        #  or we can use if mid * mid > x
-        if mid > x/mid:
-            right = mid = -1
-        else:
+        
+        mid = left + (right - left)//2
+        
+        mid_squared = mid * mid
+        
+        if mid_squared <= x:
             left = mid + 1
+        else:
+            right = mid -1
+    
     return left - 1
 
 
