91f655a54a21c708878a41a2e842e053b98145f7
unknown <umair.stem@gmail.com>
2020-07-21 09:54:53 -0400

    Home Robber

--- LeetCode/topics/dynamic/homeRobbery.py
+++ LeetCode/topics/dynamic/homeRobbery.py
@@ -1,32 +1,27 @@
-# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed,
-# the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it
-#  will automatically contact the police if two adjacent houses were broken into on the same night.
-
-# Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount
-#  of money you can rob tonight without alerting the police.
-
-# Example 1:
-# Input: [1,2,3,1]
-# Output: 4
-# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-# Total amount you can rob = 1 + 3 = 4.
+# https://leetcode.com/problems/house-robber/
 
 
-# Example 2:
-# Input: [2,7,9,3,1]
-# Output: 12
-# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-#              Total amount you can rob = 2 + 9 + 1 = 12.
-
-nums = 0
 
 
+# Time O (n) | Space O(n)
 def rob(nums):
 
-    last, now = 0, 0
+    nums = [0] + nums
 
-    for num in nums:
-        last, now = now, max(last + num, now)
+    for i in range(2, len(nums)):
+        nums[i] = max(nums[i-1], nums[i-2] + nums[i])
+    return nums[-1]
+
+# Time O(n) | Space O(1)
+def robII(nums):
+    prevMax = 0
+    currMax = 0
+    for x in nums:
+        temp = currMax;
+        currMax = max(prevMax + x, currMax)
+        prevMax = temp
+        
+    return currMax
 
 
 nums2 = [2, 1, 1, 2]
