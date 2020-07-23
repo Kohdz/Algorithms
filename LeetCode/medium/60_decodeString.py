a73f14b5385291e44858de362bbce9bb08c5248d
unknown <umair.stem@gmail.com>
2020-07-23 09:23:24 -0400

    Decode String

---
+++ LeetCode/medium/60_decodeString.py
@@ -0,0 +1,22 @@
+
+def decodeString(s):
+    
+    
+    stack, curNum, curString = [], 0, ''
+    for c in s:
+        if c == '[':
+            stack.append((curString, curNum))
+            curString, curNum = '', 0
+        elif c == ']':
+            prevString, num = stack.pop()
+            curString = prevString + num*curString
+        elif c.isdigit():
+            curNum = curNum*10 + int(c)
+        else:
+            curString += c
+    return curString
+
+
+s = "3[a]2[bc]"
+# Output: "aaabcbc"
+print(decodeString(s))
