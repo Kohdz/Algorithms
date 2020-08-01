3fb291d42ff9194ff40cf352b49ac7d8389f97b7
unknown <umair.stem@gmail.com>
2020-08-01 18:48:23 -0400

    Copy List With Random Pointer

--- AmazonOA/randomPointers.py
+++ AmazonOA/randomPointers.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/copy-list-with-random-pointer/
-
+# https://www.youtube.com/watch?v=viKm998YxFc
+# https://www.youtube.com/watch?v=OvpKeraoxW0
 
 # Definition for a Node.
 class Node:
@@ -8,8 +9,60 @@ class Node:
         self.next = next
         self.random = random
 
+def copyRandomList(head):       
+        # Time O(n) | Space O(n)
+        
+        hash_map = {}
+        
+        # create dummy node
+        dummyHead = result = Node(0)
+        
+        current = head
 
-def copyRandomList(head):
+        # create a copy of all the nodes
+        # link the copeid nodes to each other
+        # use hash_map to link copied nodes to orginal nodes
+        while current:
+            
+            # create new node
+            NewNode = Node(current.val)
+            
+            # map/link the old node to the new node
+            hash_map[current]= NewNode
+            
+            # dummy node points to current node
+            dummyHead.next = NewNode
+            
+            # move both current and dummy next
+            dummyHead = dummyHead.next
+            current = current.next
+            
+            
+        current = head
+        
+        # link random pointers in the copied nodes
+        while current:
+            
+            # get the copied nodes from out link/map
+            copiedNode = hash_map[current]
+            
+            # get the random value
+            random = current.random
+           
+            # if random exists, 
+            if random is not None:
+                
+                # connect copied node to copied node
+                copiedRandom = hash_map[random]
+                copiedNode.random = copiedRandom
+            else:
+                # there is no random and set it to random
+                copiedNode.random = None
+            current = current.next
+            
+        return result.next
+                
+def copyRandomListII(head):
 
     node_hash = {}
     temp = head
