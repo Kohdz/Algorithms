c55b134a9603eb40b8e8cf26799f148a01af5675
unknown <umair.stem@gmail.com>
2020-08-17 18:02:47 -0400

    Meeting Rooms

--- LeetCode/blind/intervals/meetingRooms.py
+++ LeetCode/blind/intervals/meetingRooms.py
@@ -1,13 +1,16 @@
-# [Meeting Rooms]
-# Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
-# determine if a person could attend all meetings.
+# https://leetcode.com/problems/meeting-rooms/
 
-# Input: [[0,30],[5,10],[15,20]]
-# Output: false
+'''
+[Meeting Rooms]
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), 
+determine if a person could attend all meetings.
 
-# Input: [[7,10],[2,4]]
-# Output: true
+Input: [[0,30],[5,10],[15,20]]
+Output: false
 
+Input: [[7,10],[2,4]]
+Output: true
+'''
 
 def AttendMeeting(times):
     start = []
