1c6132439f8d5cb28cd784dbc0863caf11a0adf7
unknown <umair.stem@gmail.com>
2020-08-27 09:45:55 -0400

    Meeting Rooms II

--- LeetCode/blind/intervals/meetingRoomsII.py
+++ LeetCode/blind/intervals/meetingRoomsII.py
@@ -1,15 +1,31 @@
-# Given an array of meeting time intervals consting of start and end times 
-# [[s1, e1], [s2, e2]...] (si < ei), find the minimum number of conferance
-# rooms required
+'''Given an array of meeting time intervals consting of start and end times 
+[[s1, e1], [s2, e2]...] (si < ei), find the minimum number of conferance
+rooms required
 
-# Example 1
-# Input: [[0, 30], [5, 10], [15, 20]
-# Output = 2
+Example 1
+Input: [[0, 30], [5, 10], [15, 20]
+Output = 2
 
-# Example 2
-# Input: [[7, 10], [2, 4]]
-# Output: 1
+Example 2
+Input: [[7, 10], [2, 4]]
+Output: 1'''
 
+'''
+https://www.youtube.com/watch?v=9ZsUM1ed05c
+https://raw.githubusercontent.com/Kohdz/LeetcodeImages/master/253_01.png?token=AD3LTZ35KNLVQAF7R7DYGPK7I62HQ
+Solution:
+
+1. Sort the input array
+2. Create a min-heap to store the end time of meetings that have had rooms
+assigned to 
+3. Start by pushing the end time of the first meeting to the heap
+4. Loop through the rest of the array
+    - compare the start time of the current meeting with the top element of the
+    min heap
+    - if they don't overlap, pop that element out from heap
+    - push the end time of the current meeting into the min heap
+5. After looping through all meeting, return the lenght of the heap
+'''
 
 import heapq
 
@@ -19,21 +35,19 @@ def minMeetingRooms(intervals):
     if not intervals:
         return 0
 
-    # process each meeting in increasig start times for intervals
-    intervals.sort(key = lambda x: x[0]) # O (n log n ) where n is number of elements in list
+    intervals.sort() 
 
-    # room heap to keep track of O (n log n) runtime where n is 
-    rooms = [intervals[0][1]]
-
-    # number of elements in intervals
-    for meeting in intervals[1:]:
+    roomsUsed = [] 
+    heapq.heappush(roomsUsed, intervals[0][1])
+    
+    for i in range(1, len(intervals)):
         # check if earliest meeting that ends in room, will finish before current meeting
         # if so, then remove earliest meeting
-        if rooms[0] <= meeting[0]:
-            heapq.heappop(rooms)
-        heapq.heappush(rooms, meeting[1])
+        if roomsUsed[0] <= intervals[i][0]:
+            heapq.heappop(roomsUsed)
+        heapq.heappush(roomsUsed, intervals[i][1])
 
-    return len(rooms)
+    return len(roomsUsed)
 
 
 
