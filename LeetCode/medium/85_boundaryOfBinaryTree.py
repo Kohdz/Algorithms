806802e02bb8b5d7099a3b7d299642f781f92f3b
unknown <umair.stem@gmail.com>
2020-08-31 11:33:23 -0400

    Boundary Of Binary Tree

--- LeetCode/medium/85_boundaryOfBinaryTree.py
+++ LeetCode/medium/85_boundaryOfBinaryTree.py
@@ -1,35 +1,48 @@
 # https://leetcode.com/problems/boundary-of-binary-tree/
+'''
+Given a binary tree, return the values of its boundary in anti-clockwise direction starting from root. 
+Boundary includes left boundary, leaves, and right boundary in order without duplicate nodes. 
+(The values of the nodes may still be duplicates.)
 
-# Given a binary tree, return the values of its boundary in anti-clockwise direction starting from root. 
-# Boundary includes left boundary, leaves, and right boundary in order without duplicate nodes. 
-# (The values of the nodes may still be duplicates.)
+Left boundary is defined as the path from root to the left-most node. Right boundary is defined as the 
+path from root to the right-most node. If the root doesn’t have left subtree or right subtree, then the 
+root itself is left boundary or right boundary. Note this definition only applies to the input binary tree, and not applies to any subtrees.
 
-# Left boundary is defined as the path from root to the left-most node. Right boundary is defined as the 
-# path from root to the right-most node. If the root doesn’t have left subtree or right subtree, then the 
-# root itself is left boundary or right boundary. Note this definition only applies to the input binary tree, and not applies to any subtrees.
+The left-most node is defined as a leaf node you could reach when you always firstly travel to the left subtree if exists. 
+If not, travel to the right subtree. Repeat until you reach a leaf node.
 
-# The left-most node is defined as a leaf node you could reach when you always firstly travel to the left subtree if exists. 
-# If not, travel to the right subtree. Repeat until you reach a leaf node.
+The right-most node is also defined by the same way with left and right exchanged.
 
-# The right-most node is also defined by the same way with left and right exchanged.
+Example 1
+Input:
+    1
+    |
+    2
+   / \
+  3   4
 
-# Example 1
-# Input:
-#     1
-#     |
-#     2
-#    / \
-#   3   4
+Ouput:
+[1, 3, 4, 2]
 
-# Ouput:
-# [1, 3, 4, 2]
+Example 2
+Input:
+     ______ 1 _______
+    /                \
+   2                  3
+ /  \                /
+4   5               6
+   / \             / \
+  7   8           9  10
 
-# Explanation:
-# The root doesn't have left subtree, so the root itself is left boundary.
-# The leaves are node 3 and 4.
-# The right boundary are node 1,2,4. Note the anti-clockwise direction means you should output reversed right boundary.
-# So order them in anti-clockwise without duplicates and we have [1,3,4,2].
+Output:
+[1, 2, 4, 7, 8, 9, 10, 6, 3]
 
+Explanation:
+The root doesn't have left subtree, so the root itself is left boundary.
+The leaves are node 3 and 4.
+The right boundary are node 1,2,4. Note the anti-clockwise direction means you should output reversed right boundary.
+So order them in anti-clockwise without duplicates and we have [1,3,4,2].
+'''
 
 # Definition for a Node.
 class Node:
