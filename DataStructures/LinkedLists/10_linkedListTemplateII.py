1ed91aa7a2d05d2e9b750dcc4691ec803841bb02
unknown <umair.stem@gmail.com>
2020-10-02 19:46:54 -0400

    Linked List Template II

---
+++ DataStructures/LinkedLists/10_linkedListTemplateII.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end='')
+            temp = temp.next
+        print()
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
