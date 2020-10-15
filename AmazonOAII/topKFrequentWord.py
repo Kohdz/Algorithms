c664284991794b23d135a843404fa1e87544ebda
kohdz <umair.stem@gmail.com>
2020-10-14 23:16:48 -0400

    Top K Frequent Word

---
+++ AmazonOAII/topKFrequentWord.py
@@ -0,0 +1,54 @@
+import heapq
+from collections import Counter
+
+
+def topkfrequentII(keywords, reviews, k):
+    elem = {}
+    for review in reviews:
+        for key in keywords:
+            if key.lower() in review.lower():
+                elem[key] = elem.get(key, 0)+1
+
+    heap = []
+    for key, freq in elem.items():
+        heap.append((-freq, key))
+
+    heapq.heapify(heap)
+    return [heapq.heappop(heap)[1] for _ in range(k)]
+
+
+def topKFrequent(keywords, reviews, k):
+
+    heap = []
+    totalWords = []
+
+    for review in reviews:
+        words = set(review.lower().split())
+        totalWords.extend(words)
+
+    wordFreq = Counter(totalWords)
+    for word, freq in wordFreq.items():
+        if word in keywords:
+            heap.append((-freq, word))
+
+    heapq.heapify(heap)
+    return [heapq.heappop(heap)[1] for _ in range(k)]
+
+    # print(totalWords)
+
+
+k = 2
+keywords = ["anacell", "cetracular", "betacellular"]
+reviews = [
+    "Anacell provides the best services in the city",
+    "betacellular has awesome services",
+    "Best services provided by anacell, everyone should use anacell",
+]
+
+# Output:
+# ["anacell", "betacellular"]
+
+# Explanation:
+# "anacell" is occuring in 2 different reviews and "betacellular" is only occuring in 1 review.
+
+print(topKFrequent(keywords, reviews, k))
